a6acce50b6c945cfa844cd4dd3b6866b9840f326
stalwartBunny <constructingintelligence@gmail.com>
2022-05-26 15:32:15 -0500

    updated 13 to accept an input

--- ex13.py
+++ ex13.py
@@ -6,3 +6,11 @@ print("The script is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+
+while True:
+    somethingImportant = input("Please submit the shadowy secrets of the soul.")
+    if somethingImportant == "Mirrors" or somethingImportant == "mirrors":
+        print("An unkind word.")
+    elif somethingImportant != "Mirrors" or somethingImportant != "mirrors":
+        print("These are not your lands.")
+    break
