1f63e614b01c10d0b2edb7caef0735c070899416
stalwartBunny <constructingintelligence@gmail.com>
2022-05-28 19:36:44 -0500

    updating for science

---
+++ catCheshire.py
@@ -0,0 +1,7 @@
+#“Alice asked the Cheshire Cat, who was sitting in a tree, “What road do I take?”
+
+#The cat asked, “Where do you want to go?”
+
+#“I don’t know,” Alice answered.
+
+#“Then,” said the cat, “it really doesn’t matter, does it?”
